0d7b2631fb9cf8b7953e17397960be45c55d30ac
dfassf <dfassf@gmail.com>
2021-10-01 12:49:03 +0900

    beforepulling

--- back/dbinit.sql
+++ back/dbinit.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `transaction` (
   `b_orderid` int(11) NOT NULL,
   `b_amount` int(11) NOT NULL,
   `b_commission` int(11) NOT NULL,
-  `price` int(11) NOT NULL,
+  `price` FLOAT(11) NOT NULL,
   `txid` varchar(150) DEFAULT NULL,
   `reg_date` datetime NOT NULL,
   PRIMARY KEY (`id`)
@@ -17,23 +17,21 @@ CREATE TABLE IF NOT EXISTS `transaction` (
 
 CREATE TABLE IF NOT EXISTS `asset` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `input` varchar(255) NOT NULL,
-  `output` varchar(10) NOT NULL,
+  `user_idx` INT(11) NOT NULL,
+  `input` FLOAT(11) NOT NULL,
+  `output` FLOAT(11) NOT NULL,
   `reg_date` datetime NOT NULL,
-  `transactionId` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `transactionId` (`transactionId`),
-  CONSTRAINT `asset_ibfk_1` FOREIGN KEY (`transactionId`) REFERENCES `transaction` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `order` (
-	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`user_idx` INT(11) NOT NULL,
-	`price` INT(11) NOT NULL,
-	`qty` INT(11) NOT NULL,
-	`order_type` INT(11) NOT NULL,
-	`active` INT(11) NOT NULL,
-	PRIMARY KEY (`id`)
+   `id` INT(11) NOT NULL AUTO_INCREMENT,
+   `user_idx` INT(11) NOT NULL,
+   `price` INT(11) NOT NULL,
+   `qty` INT(11) NOT NULL,
+   `order_type` INT(11) NOT NULL,
+   `active` INT(11) NOT NULL,
+   PRIMARY KEY (`id`)
 )
 ENGINE=InnoDB AUTO_INCREMENT=1;
 
@@ -44,4 +42,3 @@ CREATE TABLE IF NOT EXISTS `user` (
   `user_pw` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
-

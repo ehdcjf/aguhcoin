6b8b36371411b2db639aac6b149d678346f523ba
ehdcjf <kdch0823@naver.com>
2021-10-01 17:10:46 +0900

    DB update create coin table

--- back/dbinit.sql
+++ back/dbinit.sql
@@ -1,6 +1,5 @@
-CREATE database IF NOT EXISTS `exchange`;
+CREATE DATABASE IF NOT EXISTS `exchange`;
 USE `exchange`;
-
 
 CREATE TABLE IF NOT EXISTS `asset` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -12,6 +11,24 @@ CREATE TABLE IF NOT EXISTS `asset` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 
+CREATE TABLE IF NOT EXISTS `coin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_idx` int(11) NOT NULL,
+  `c_input` int(11) NOT NULL,
+  `c_output` int(11) NOT NULL,
+  `coin_date` datetime DEFAULT current_timestamp(),
+  `coin_id` int(11) DEFAULT 1,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `coininfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `unit` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+INSERT INTO `coininfo` (`id`, `name`, `unit`) VALUES (1, 'aguhcoin', 'AGU');
 
 CREATE TABLE IF NOT EXISTS `order` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -21,6 +38,7 @@ CREATE TABLE IF NOT EXISTS `order` (
   `order_type` int(11) NOT NULL,
   `leftover` int(11) NOT NULL,
   `order_date` datetime DEFAULT current_timestamp(),
+  `coin_id` int(11) DEFAULT 1,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
@@ -41,48 +59,20 @@ CREATE TABLE IF NOT EXISTS `transaction` (
   `b_orderid` int(11) NOT NULL,
   `b_amount` int(11) NOT NULL,
   `b_commission` int(11) NOT NULL,
-<<<<<<< HEAD
   `price` float NOT NULL,
-=======
-  `price` FLOAT(11) NOT NULL,
->>>>>>> dba086f431cd0bfc40074c1c070075d61e611b11
   `txid` varchar(150) DEFAULT NULL,
   `tx_date` datetime DEFAULT current_timestamp(),
+  `coin_id` int(11) DEFAULT 1,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-<<<<<<< HEAD
-=======
-CREATE TABLE IF NOT EXISTS `asset` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_idx` INT(11) NOT NULL,
-  `input` FLOAT(11) NOT NULL,
-  `output` FLOAT(11) NOT NULL,
-  `reg_date` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS `order` (
-   `id` INT(11) NOT NULL AUTO_INCREMENT,
-   `user_idx` INT(11) NOT NULL,
-   `price` INT(11) NOT NULL,
-   `qty` INT(11) NOT NULL,
-   `order_type` INT(11) NOT NULL,
-   `active` INT(11) NOT NULL,
-   PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB AUTO_INCREMENT=1;
-
->>>>>>> dba086f431cd0bfc40074c1c070075d61e611b11
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` varchar(50) NOT NULL,
   `user_pw` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-<<<<<<< HEAD
 ) ENGINE=InnoDB AUTO_INCREMENT=301 DEFAULT CHARSET=utf8mb4;
-
 
 INSERT INTO user (user_id, user_pw) VALUES ('0000', '1234');
 INSERT INTO asset (user_idx, input, output, asset_date) VALUES (0000,1000000,0,NOW());
@@ -684,7 +674,3 @@ INSERT INTO user (user_id, user_pw) VALUES ('0298', '1234');
 INSERT INTO asset (user_idx, input, output, asset_date) VALUES (0298,1000000,0,NOW());
 INSERT INTO user (user_id, user_pw) VALUES ('0299', '1234');
 INSERT INTO asset (user_idx, input, output, asset_date) VALUES (0299,1000000,0,NOW());
-
-=======
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
->>>>>>> dba086f431cd0bfc40074c1c070075d61e611b11

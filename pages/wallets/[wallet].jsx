f6c21f13823343bcec420407e621298c0cb9d06a
GUMUNYEONG <csprout@nate.com>
2021-10-06 09:31:46 +0900

    git

---
+++ pages/wallets/[wallet].jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import MainLayout from '../../components/MainLayout';
+
+const Wallet = () => {
+    return(
+        <>
+            <Head>
+                <title>악어코인 | 내 지갑</title>
+            </Head>
+            <MainLayout>
+                내 지갑 화면 작업중
+            </MainLayout>
+        </>
+    );
+}
+
+export default Wallet;

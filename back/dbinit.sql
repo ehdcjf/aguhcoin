e6b754c97ced5f17039fdec0413f660469a0d919
dfassf <dfassf@gmail.com>
2021-10-06 09:26:19 +0900

    usercontroller upload

--- back/dbinit.sql
+++ back/dbinit.sql
@@ -1,6 +1,15 @@
 CREATE DATABASE IF NOT EXISTS `exchange`;
 USE `exchange`;
 
+CREATE TABLE IF NOT EXISTS `user` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`user_id` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`user_pw` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+  `user_wallet` VARCHAR(255) NULL COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`id`) USING BTREE,
+)
+COLLATE='utf8mb4_general_ci' ENGINE=InnoDB AUTO_INCREMENT=1;
+
 CREATE TABLE IF NOT EXISTS `order_list` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`user_idx` INT(11) NOT NULL,

045c85d8f25494847a827bf2d78f69a8d1fa7e88
GUMUNYEONG <csprout@nate.com>
2021-10-07 14:49:12 +0900

    steal chart

--- front/components/Volume.jsx
+++ front/components/Volume.jsx
@@ -1,49 +1,52 @@
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 import styles from '../styles/Volume.module.css'
 
-const Volume = () =>{
-    return(
+const Volume = () => {
+    const { txList } = useSelector((state) => state.exchange)
+
+    function renderTxList() {
+        if (txList.length > 0) {
+            return (
+                txList.slice(0,10).map((e, k) => {
+                    return (
+                        <tr>
+                            <td>{e.tx_date}</td>
+                            <td>{e.price}</td>
+                            <td>{e.buy_commission}</td>
+                            <td>{e.price * e.buy_commission}</td>
+                        </tr>
+                    )
+                })
+            )
+        } else {
+            return (
+                <div>없음</div>
+            )
+        }
+    }
+    useEffect(() => {
+        renderTxList();
+    })
+    return (
         <>
-            
             <div className={styles.volume}>
                 <span><a>체결</a></span>
                 <table className={styles.table}>
-                <thead>
-                    <tr>
-                        <td>체결시간</td>
-                        <td>체결가격</td>
-                        <td>체결량(AGU)</td>
-                        <td>체결금액(KRW)</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>10.05 <a>08:29</a></td>
-                        <td>60,112,000</td>
-                        <td>0.0192054</td>
-                        <td>299,945</td>
-                    </tr>
-                    <tr>
-                        <td>10.05 <a>08:30</a></td>
-                        <td>60,416,000</td>
-                        <td>0.0190072</td>
-                        <td>299,945</td>
-                    </tr>
-                    <tr>
-                        <td>10.05 <a>08:32</a></td>
-                        <td>60,194,000</td>
-                        <td>0.0147072</td>
-                        <td>299,945</td>
-                    </tr>
-                    <tr>
-                        <td>10.05 <a>08:35</a></td>
-                        <td>60,830,000</td>
-                        <td>0.0230072</td>
-                        <td>299,945</td>
-                    </tr>
-                </tbody>
-            </table>
+                    <thead>
+                        <tr>
+                            <td>체결시간</td>
+                            <td>체결가격</td>
+                            <td>체결량(AGU)</td>
+                            <td>체결금액(KRW)</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {renderTxList()}
+                    </tbody>
+                </table>
             </div>
-            
+
 
         </>
     )

69f30575d44679ac426718c8b1b0764f8679eba8
hwan <82621358+proazel@users.noreply.github.com>
2021-10-10 16:52:32 +0900

    미체결내역(/user/nontd) 추가

--- back/routes/user/index.js
+++ back/routes/user/index.js
@@ -11,4 +11,6 @@ router.post('/join', userController.createUser)
 // 유저 거래내역, 미체결 내역
 router.post('/txlog', userController.txHistory)
 router.post('/outstandinglog', userController.outstandingLog)
+// 지환 추가, 미체결 내역
+router.post('/nontd',userController.nontd);
 module.exports = router
